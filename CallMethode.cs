2aa5739e48d9f595beb4e0ddb146531c9d6b9b0a
d00h4n <doohan.mintarso@gmail.com>
2023-05-09 16:54:22 +0700

    UPDATE 2

---
+++ CallMethode.cs
@@ -0,0 +1,36 @@
+// namespace Program.cs
+// {
+//     class Program
+// {
+//     static void Main() {
+//         Cat cotton = new Cat( ); //Create instance from Blueprint (Class)
+//         Cat cotton2 = new Cat("Cotton",5, 100); //Create instance from Blueprint (Class)
+        
+//         int x = cotton.GetSalary();
+//         cotton.SetSalary(5);
+//         Console.WriteLine(x);
+//         Console.WriteLine(cotton.GetSalary());
+        
+//         cotton.Eat(); //Call method
+//         cotton.Sleep();
+//         Console.WriteLine(cotton.age);
+//         cotton.age = 1; //Assign Value
+//         cotton.name = "Cotton";
+//         cotton.race = "Anggora";
+        
+//         Console.WriteLine(cotton.age); //Call value
+//         Console.WriteLine(cotton.name);
+//         Console.WriteLine(cotton.race);
+//         Console.WriteLine($"Cat name is {cotton.name} age {cotton.age} race is  {cotton.race}");
+//         Console.WriteLine("Cat name is {cotton.name} age {cotton.age} race is  {cotton.race}");
+//         Console.WriteLine("Cat name is \n " + cotton.name + " age " + cotton.age + " race is " + cotton.race);
+//         Console.WriteLine(@"To make new line use \n .");
+//         Console.WriteLine(@"FilePath is = C:\Computer\Windows\Users\");
+
+//         // Bird Belang = new Bird();
+//         // Belang.name = "Belang";
+//         // Console.WriteLine(Belang.name);
+
+//     }
+// } 
+// }
