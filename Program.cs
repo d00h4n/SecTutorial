11cf16aa76323bb480998fe12b0e1f2c80b55f9a
d00h4n <doohan.mintarso@gmail.com>
2023-05-09 16:08:27 +0700

    Edit1

--- Program.cs
+++ Program.cs
@@ -1,89 +1,89 @@
 ﻿
 
-// class Cat
-// {
-//     //Variable
-//     public int age;
-//     public string name;
-//     public string race;
-//     private int _salary;
+class Cat
+{
+    //Variable
+    public int age;
+    public string name;
+    public string race;
+    private int _salary;
     
-//     //Constructor
-//     public Cat() {}
-//     public Cat(string a,  int b, int c) {
-//         name = a;
-//         age = b;
-//         _salary = c;
-//     }
+    //Constructor
+    public Cat() {}
+    public Cat(string a,  int b, int c) {
+        name = a;
+        age = b;
+        _salary = c;
+    }
 
-//     //Method
-//     public void Eat() {
-//         Console.WriteLine("Cat Eat");
-//     }
+    //Method
+    public void Eat() {
+        Console.WriteLine("Cat Eat");
+    }
 
-//     public void Sleep() {
-//         Console.WriteLine("Cat Sleep");
-//     }
+    public void Sleep() {
+        Console.WriteLine("Cat Sleep");
+    }
     
-//     //Method with Parameter
-//     public void SetSalary(int x) {
-//         _salary = x;
-//     }
+    //Method with Parameter
+    public void SetSalary(int x) {
+        _salary = x;
+    }
     
-//     public int GetSalary() {
-//         return _salary;
-//     }
-// }   
+    public int GetSalary() {
+        return _salary;
+    }
+}   
 
-// class Program
-// {
-//     static void Main() {
-//         Cat cotton = new Cat( ); //Create instance from Blueprint (Class)
-//         Cat cotton2 = new Cat("Cotton",5, 100); //Create instance from Blueprint (Class)
+class Program
+{
+    static void Main() {
+        Cat cotton = new Cat( ); //Create instance from Blueprint (Class)
+        Cat cotton2 = new Cat("Cotton",5, 100); //Create instance from Blueprint (Class)
         
-//         int x = cotton.GetSalary();
-//         cotton.SetSalary(5);
-//         Console.WriteLine(x);
-//         Console.WriteLine(cotton.GetSalary());
+        int x = cotton.GetSalary();
+        cotton.SetSalary(5);
+        Console.WriteLine(x);
+        Console.WriteLine(cotton.GetSalary());
         
-//         cotton.Eat(); //Call method
-//         cotton.Sleep();
-//         Console.WriteLine(cotton.age);
-//         cotton.age = 1; //Assign Value
-//         cotton.name = "Cotton";
-//         cotton.race = "Anggora";
+        cotton.Eat(); //Call method
+        cotton.Sleep();
+        Console.WriteLine(cotton.age);
+        cotton.age = 1; //Assign Value
+        cotton.name = "Cotton";
+        cotton.race = "Anggora";
         
-//         Console.WriteLine(cotton.age); //Call value
-//         Console.WriteLine(cotton.name);
-//         Console.WriteLine(cotton.race);
-//         Console.WriteLine($"Cat name is {cotton.name} age {cotton.age} race is  {cotton.race}");
-//         Console.WriteLine("Cat name is {cotton.name} age {cotton.age} race is  {cotton.race}");
-//         Console.WriteLine("Cat name is \n " + cotton.name + " age " + cotton.age + " race is " + cotton.race);
-//         Console.WriteLine(@"To make new line use \n .");
-//         Console.WriteLine(@"FilePath is = C:\Computer\Windows\Users\");
-//     }
-// } 
+        Console.WriteLine(cotton.age); //Call value
+        Console.WriteLine(cotton.name);
+        Console.WriteLine(cotton.race);
+        Console.WriteLine($"Cat name is {cotton.name} age {cotton.age} race is  {cotton.race}");
+        Console.WriteLine("Cat name is {cotton.name} age {cotton.age} race is  {cotton.race}");
+        Console.WriteLine("Cat name is \n " + cotton.name + " age " + cotton.age + " race is " + cotton.race);
+        Console.WriteLine(@"To make new line use \n .");
+        Console.WriteLine(@"FilePath is = C:\Computer\Windows\Users\");
+    }
+} 
 
 
-// Getter and setter
-// class Bird
-// {
-//   public string name   
+Getter and setter
+class Bird
+{
+  public string name   
   
-//   { get; set; }
+  { get; set; }
   
-// }
+}
 
-// class Program {
-//  static void Main(string[] args)
-//   {
-//     Bird Belang = new Bird();
-//     Belang.name = "Belang";
-//     Console.WriteLine(Belang.name);
-// }
-// }
+class Program {
+ static void Main(string[] args)
+  {
+    Bird Belang = new Bird();
+    Belang.name = "Belang";
+    Console.WriteLine(Belang.name);
+}
+}
 
-// Getter and setter Private
+Getter and setter Private
 
 class Person
 {
